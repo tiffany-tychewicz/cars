a17f16538c8771d2e00646deb7b0b4d0732f7676
Siva <carah.tyche@gmail.com>
2019-06-27 21:52:49 -0500

    added create method and show json

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -1,7 +1,21 @@
 class Api::CarsController < ApplicationController
+  
   def index
-    @cars = cars.all
+    @cars = Car.all
     render "index.json.jb"
   end
+
+  def create
+    @car = Car.new(
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      year: params[:year]
+      )
+    @car.save
+    render "show.json.jb"
+  end
+
+
 
 end
